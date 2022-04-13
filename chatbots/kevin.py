75ee9c75a90855d2f74477555133bc8a4f703f24
Gregory Knapp <gregory.c.knapp@gmail.com>
2022-04-12 22:51:20 -0600

    Combine entire pipeline with issue with Neo4j
    Set up pipeline to take input text and generate a templated quest.
    Connected with Abbi's neo4j database query, but sometimes throws an
    exception that it found nothing in the database even though the
    template style matches the example.

--- chatbots/kevin.py
+++ chatbots/kevin.py
@@ -1,8 +1,11 @@
+from random import sample
 from chatbots.Chatbot import Chatbot
 from transformers import pipeline
 import os
 import spacy
 from spacy.language import Language
+from chatbots.fill_temp import fill_templates
+import re
 
 # Adam's Spacy pipeline
 @Language.component("templater_component")
@@ -35,40 +38,72 @@ nlp = spacy.load("en_core_web_lg")
 nlp.add_pipe("templater_component", name="templater", last=True)
 
 
+
 class Kevin(Chatbot):
     def __init__(self):
         #print(os.listdir())
         self.generator = pipeline('text-generation', model='./chatbots/gpt2-untemplated-quests', tokenizer='gpt2',config="chatbots\gpt2-untemplated-quests\config.json")
         self.response = None
 
+    def fix_templates(self, templated_quest):
+        template_vals = ['PERSON', 'ORG', 'LOC']
+        replace_vals = ['<PERSON1>', '<ORG1>', '<LOC1>']
+
+        for i in range(3):
+            if template_vals[i] in templated_quest:
+                templated_quest = templated_quest.replace(template_vals[i], replace_vals[i])
+
+        return templated_quest
+
     def send_message(self):
         print('\nSending message...')
 
+        # Step 1: Extract player message
         # Extract the player's message from the json
         plr_full_msg = self.response['text'].split(":")[1]
 
-        # Generate response and parse everything up to the last period to prevent incomplete sentence
-        # TODO: Is there a better way to do this?
+        # Cheat and capitalize first letter of player message
+        plr_full_msg = plr_full_msg.capitalize()
+
+        # Step 2: Generate response from GPT2
         npc_quest = self.generate(plr_full_msg).rpartition('.')
+        
+        # Parse everything up to the last period to prevent incomplete sentence
         npc_quest = npc_quest[0] + npc_quest[1]
 
-        # Use spacy pipeline to template quest
+        # Clean up spaces
+        npc_quest = npc_quest.replace('\n', ' ')
+        npc_quest = re.sub(' +', ' ', npc_quest)
+        print("\nElminate spaces generated text: ", npc_quest)
+
+        # STEP 3: Use Spacy pipeline to create templates
         doc = nlp(npc_quest)
-
-        # Convert to String
         quest_template = doc_to_string(doc)
-        print("before processing: ", quest_template)
 
-        # Add the 5->Plr message to the front of the string 
-        # TODO: Eliminate the need for this
-        quest_template = "5->Plr: " + quest_template
-
-        # Replace newlines with spaces to avoid awkward formatting from GPT2
+        # Clean up spaces in template
         quest_template = quest_template.replace('\n', ' ')
+        quest_template = re.sub(' +', ' ', quest_template)
 
         print("\nTemplated Quest: ", quest_template)
+
+        # Replace template values with ones that work with Abbi's code
+        quest_template = self.fix_templates(quest_template)
         
-        return {"text": quest_template}
+        # Clean up formatting
+        quest_template = quest_template.replace('\n', ' ')
+        quest_template = re.sub(' +', ' ', quest_template)
+
+        print("\nFixed template: ", quest_template)
+
+        # STEP 4: Fill in templates using knowledge graph
+        quest_template = "5->Plr: " + quest_template
+        #hard_coded_temp = "Armageddon approaches. Only you can stop it, with the help of <PERSON1>. You must meet them at <LOC1>." 
+
+        final_quest = fill_templates.fill_in(quest_template)
+
+        print("\nFinal Quest: ", final_quest)
+        
+        return {"text": final_quest}
 
     def recv_message(self, message):
         self.response = message
@@ -76,4 +111,8 @@ class Kevin(Chatbot):
 
     def generate(self, input):
         print("Generating message from pipeline...")
-        return self.generator(input)[0]['generated_text']
+        out = self.generator(input, )[0]['generated_text']
+
+        print("\nText output : ", out)
+        return out
+
